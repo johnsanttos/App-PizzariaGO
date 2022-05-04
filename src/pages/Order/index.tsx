d35d37fc3e51c6aa8305173e001a205bc46a5c33
John <jhow.mito11@gmail.com>
2022-05-04 10:26:21 -0300

    Order screen with parameters

---
+++ src/pages/Order/index.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TouchableOpacity,
+  TextInput
+ } from 'react-native'
+
+import { useRoute, RouteProp } from '@react-navigation/native'
+
+import { Feather } from '@expo/vector-icons'
+
+//tipagem para receber parametros de outra tela
+type RouteDetailParams = {
+  Order:{
+    number: string | number;
+    order_id: string;
+  }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
+
+export default function Order(){
+  const route = useRoute<OrderRouteProps>();
+
+  return(
+    <View style={styles.container}>
+      
+      <View style={styles.header}>
+        <Text style={styles.title}>Mesa {route.params.number}</Text>
+        <TouchableOpacity>
+          <Feather name="trash-2" size={28} color="#FF3F4b" />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.input}>
+        <Text style={{ color: '#FFF' }}>Pizzas</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.input}>
+        <Text style={{ color: '#FFF' }}>Pizza de calabresa</Text>
+      </TouchableOpacity>
+
+      <View style={styles.qtdContainer}>
+        <Text style={styles.qtdText}>Quantidade</Text>
+        <TextInput
+          style={[styles.input, { width: '60%', textAlign: 'center' } ]}
+          placeholderTextColor="#F0F0F0"
+          keyboardType="numeric"
+          value="1"
+        />
+      </View>
+
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#1d1d2e',
+    paddingVertical: '5%',
+    paddingEnd: '4%',
+    paddingStart: '4%'
+  },
+  header:{
+    flexDirection: 'row',
+    marginBottom: 12,
+    alignItems: 'center',
+    marginTop: 24,
+  },
+  title:{
+    fontSize:30,
+    fontWeight: 'bold',
+    color: '#FFF',
+    marginRight: 14
+  },
+  input:{
+    backgroundColor: '#101026',
+    borderRadius: 4,
+    width: '100%',
+    height: 40,
+    marginBottom: 12,
+    justifyContent: 'center',
+    paddingHorizontal: 8,
+    color: '#FFF',
+    fontSize: 20,
+  },
+  qtdContainer:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-between'
+  },
+  qtdText:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFF'
+  }
+})
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,13 +2,30 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import Order from '../pages/Order';
 
-const Stack = createStackNavigator ();
+export type StackParamsList ={
+    Dashboard: undefined;
+    Order: {
+    number: number | string;
+    order_id: string
+  }
+}
+
+const Stack = createStackNavigator <StackParamsList>();
 
 function AppRoutes(){
   return(
     <Stack.Navigator>
-      <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Screen 
+      name="Dashboard" 
+      component={Dashboard} 
+      options={{ headerShown: false }} />
+
+      <Stack.Screen 
+      name="Order" 
+      component={Order} 
+      options={{ headerShown: false }} />
     </Stack.Navigator>
   )
 }
